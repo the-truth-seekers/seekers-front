ea3d1c959f82ac021ddb47beae0971c85ac8c196
DouglasNova <douglas.nova@bandtec.com.br>
2023-04-24 19:52:58 -0300

    feat: mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ConsultaComponent } from './paginas/consulta/consulta.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     ConsultaComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/paginas/consulta/consulta.component.ts
+++ src/app/paginas/consulta/consulta.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.component.html',
   styleUrls: ['./consulta.component.scss']
 })
-export class ConsultaComponent implements OnInit {
+export class ConsultaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
-  constructor() { }
+  url: string | undefined;
+  resultadoBool: boolean | undefined;
+  resultadoPorcentagem: number | undefined;
 
-  ngOnInit(): void {
-  }
-  exec(){
-    alert("teste")
+  onSubmit(): void {
+    if(this.url == "https://g1.globo.com/mundo/noticia/2023/04/23/dragao-de-10-metros-pega-fogo-durante-show-em-parque-da-disney-na-california-video.ghtml"){
+      this.resultadoBool = false;
+      this.resultadoPorcentagem = Math.floor(Math.random() * (99 - 80 + 1)) + 80
+    }else{
+      this.resultadoBool = true;
+      this.resultadoPorcentagem = Math.floor(Math.random() * (60 - 20 + 1)) + 20
+    }
+
   }
 
 }
